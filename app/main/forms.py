719256101a4c2b6b34ae3a4633b1cbc750e55290
shureim <tarickaliabdi@gmail.com>
2018-10-14 15:04:28 +0300

    created class pitchform  in the main forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,13 @@
-# from flask_wtf import FlaskForm
-# from wtforms import TextAreaField,SelectField,SubmitField,
-# from wtforms.validators import Required
-#
-# class PitchForm(FlaskForm):
-#
-#     title = TextAreaField('pitch',validators=[Required()])
-#     my_category = SelectField('category',desired=[('interview','interview'),('product','product'),('job','job'),('pick-up-line','pick-up-line')], validators=[Required()])
-#     submit = SubmitField('Submit')
-#
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField,SelectField,SubmitField,
+from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+
+    title = TextAreaField('pitch',validators=[Required()])
+    my_category = SelectField('category',desired=[('interview','interview'),('product','product'),('job','job'),('pick-up-line','pick-up-line')], validators=[Required()])
+    submit = SubmitField('Submit')
+
 # class CommentForm(FlaskForm):
 #     comment = TextAreaField('comment on your desired pitch', validators=[Required()])
 #     submit = SubmitField('Comment')
