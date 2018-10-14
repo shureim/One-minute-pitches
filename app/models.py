f0a51ceb9f331607fa25f30b1d7dbab1ba089935
shureim <tarickaliabdi@gmail.com>
2018-10-14 15:16:44 +0300

    added  a class comment in the models.py

--- app/models.py
+++ app/models.py
@@ -66,25 +66,25 @@ class Pitch(db.Model):
 
 
 
-# class Comment(db.Model):
-#     __tablename__ = 'comments'
-#
-#     id = db.Column(db.Integer,primary_key=True)
-#     comment_content= db.Column(db.String())
-#     pitch_id = db.Column(db.Integer)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#
-#     def save_comment(self):
-#         db.session.add(self)
-#         db.session.commit()
-#
-#     @classmethod
-#     def get_comments(cls, id):
-#         comments = Comment.query.filter_by(pitch_id=id).all()
-#         return comments
-#
-#     def __repr__(self) :
-#         return f'User {self.name}'
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key=True)
+    comment_content= db.Column(db.String())
+    pitch_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
+
+    def __repr__(self) :
+        return f'User {self.name}'
 #
 # class UpVote(db.Model):
 #     __tablename__ = 'upvotes'
