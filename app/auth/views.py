c98946bff535b6725d7d1542898b4f66d95aa7be
shureim <tarickaliabdi@gmail.com>
2018-10-14 14:55:58 +0300

    created the login method in the auth view.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,26 +1,26 @@
-# from flask import render_template,redirect,url_for,flash,request
-# from flask_login import login_user,logout_user,login_required
-# from . import auth
-# from ..models import User
-# from .forms import LoginForm,RegistrationForm
-# from ..import db
-# from ..email import mail_message
-#
-# @auth.route('/login',methods=['GET','POST'])
-# def login():
-#     login_form = LoginForm()
-#     if login_form.validate_on_submit():
-#         user = User.query.filter_by(email= login_form.email.data).first()
-#         if user is not None and user.verify_password(login_form.password.data):
-#             login_user(user,login_form.remember.data)
-#             return redirect(request.args.get('next') or url_for('main.home'))
-#
-#         flash('Invalid Username or Password')
-#
-#     title ="Pitch Login"
-#     return render_template('auth/login.html',login_form=login_form,title=title)
-#
-#
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user,logout_user,login_required
+from . import auth
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from ..import db
+from ..email import mail_message
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email= login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.home'))
+
+        flash('Invalid Username or Password')
+
+    title ="Pitch Login"
+    return render_template('auth/login.html',login_form=login_form,title=title)
+
+
 # @auth.route('/logout')
 # @login_required
 # def logout():
