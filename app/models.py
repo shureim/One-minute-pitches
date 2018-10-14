641bdd83a3dcae6ecf042ff632c90b75117b74ff
shureim <tarickaliabdi@gmail.com>
2018-10-14 15:17:48 +0300

    added  a class downvote in the models.py

--- app/models.py
+++ app/models.py
@@ -104,23 +104,23 @@ class UpVote(db.Model):
 
     def __repr__(self):
         return f'{self.id_user}:{self.pitching_id}'
-#
-#
-# class DownVote(db.Model):
-#     __tablename__ = 'downvotes'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     id_user = db.Column(db.Integer, db.ForeignKey('users.id'))
-#     pitching_id = db.Column(db.Integer)
-#
-#     def save_vote(self):
-#         db.session.add(self)
-#         db.session.commit()
-#
-#     @classmethod
-#     def get_downvotes(cls, id):
-#         downvote = DownVote.query.filter_by(pitching_id=id).all()
-#         return downvote
-#
-#     def __repr__(self):
-#         return f'{self.id_user}:{self.pitching_id}'
+
+
+class DownVote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitching_id = db.Column(db.Integer)
+
+    def save_vote(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_downvotes(cls, id):
+        downvote = DownVote.query.filter_by(pitching_id=id).all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.id_user}:{self.pitching_id}'
