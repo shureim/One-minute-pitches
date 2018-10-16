2f5f450c330a8bc999ed541ffc0942ba77aebdb2
shureim <tarickaliabdi@gmail.com>
2018-10-16 12:35:45 +0300

    favicon

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Pitch, Comment
 
+
 # instances for the create_app
 app = create_app('production')
 
