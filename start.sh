1fafaf8a5d124a6c1187d206c6fc9641bbb48ce8
shureim <tarickaliabdi@gmail.com>
2018-10-16 12:04:23 +0300

    hidding start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY='100'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://tarek:pit001@localhost/minute'
-export MAIL_USERNAME=tarickaliabdi@gmail.com
-export MAIL_PASSWORD=baricha001
-
-python3.6 manage.py server
