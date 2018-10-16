880707d1d476cbe86c81eb8cdd9a2b6e6a4fbd7e
shureim <tarickaliabdi@gmail.com>
2018-10-16 09:17:02 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='100'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tarek:pit001@localhost/minute'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://tarek:pit001@localhost/minute'
 export MAIL_USERNAME=tarickaliabdi@gmail.com
 export MAIL_PASSWORD=baricha001
 
